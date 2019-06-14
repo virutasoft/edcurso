3d056100e8f61d3b5e99434ec629dd935f76df51
Krigonzk <virutasoft@gmail.com>
2019-06-13 22:45:49 -0500

    constantes y tipos de dato

---
+++ 02/02constantes/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	//const nombre = "perrito"
+	// nombre = "zorra"
+	// fmt.Println(nombre)
+	//GO no permite hacer operaciones aritmeticas entre datos con diferente tipo↓
+	//ha no ser que se le haga un Casting
+	//GO es estáticamente tipado y es muy estricto con los tipos de dato
+	// var a int
+	// var b int8
+	// n := "pedro"
+	// //p := "grajales"
+	// a = 2514
+	// b = 3
+
+	// //casting	↓↓↓
+
+	// c := a + int(b)
+
+	// //casting	↑↑↑
+	// fmt.Println(c)
+
+	// //prioridad aritmética
+
+	// //+ - * /
+	// //priorida siempre sera () % * / + -
+	// // d := 6 + 6*(6-6)
+	// // fmt.Println(d)
+
+	// // fmt.Printf("hola %s %d cómo estás?", n, b)
+
+	// // fmt.Printf("c es de tipo %T", c)
+
+	//valor cero
+	//valor inicial en 0
+
+	var nombre string
+	var numero int
+	var  entiendes bool
+	fmt.Println(nombre, numero)
+	fmt.Println(entiendes)
+
+}
